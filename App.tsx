d50f209e805ff0acafb43b13d86118edaf4d8d31
Severi <severi.lybeck@gmail.com>
2023-11-11 13:50:15 +0000

    Removed React Native Introduction screen

--- App.tsx
+++ App.tsx
@@ -78,26 +78,13 @@ function App(): JSX.Element {
 			<ScrollView
 				contentInsetAdjustmentBehavior="automatic"
 				style={backgroundStyle}>
-				<Header />
 				<View
 					style={{
 						backgroundColor: isDarkMode ? Colors.black : Colors.white,
 					}}>
-					<Section title="Step One">
-						Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-						screen and then come back to see your edits.{' '}
-						{JSON.stringify(result)}
-					</Section>
-					<Section title="See Your Changes">
-						<ReloadInstructions />
-					</Section>
-					<Section title="Debug">
-						<DebugInstructions />
-					</Section>
-					<Section title="Learn More">
-						Read the docs to discover what to do next:
-					</Section>
-					<LearnMoreLinks />
+						<Text style={styles.text}>
+							Moro
+						</Text>
 				</View>
 			</ScrollView>
 		</SafeAreaView>
@@ -105,22 +92,9 @@ function App(): JSX.Element {
 }
 
 const styles = StyleSheet.create({
-	sectionContainer: {
-		marginTop: 32,
-		paddingHorizontal: 24,
-	},
-	sectionTitle: {
-		fontSize: 24,
-		fontWeight: '600',
-	},
-	sectionDescription: {
-		marginTop: 8,
-		fontSize: 18,
-		fontWeight: '400',
-	},
-	highlight: {
-		fontWeight: '700',
+	text: {
+		fontSize: 138,
+		fontFamily: "HarmonyOS Sans, Regular"
 	},
 });
-
 export default App;
