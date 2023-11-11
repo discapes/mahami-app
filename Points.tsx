9ec697e923ee664bed8562bb959dc1d3e2880d4c
Severi <severi.lybeck@gmail.com>
2023-11-11 14:27:12 +0000

    Installed react-navigation

---
+++ Points.tsx
@@ -0,0 +1,23 @@
+import React, {useEffect, useState} from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+	SafeAreaView,
+	ScrollView,
+	StatusBar,
+	StyleSheet,
+	Text,
+	useColorScheme,
+	View,
+	NativeModules,
+} from 'react-native';
+
+
+export default function Points(): JSX.Element {
+    return(
+        <View>
+        <Text>
+            POints here
+        </Text>
+        </View>
+    );
+}
