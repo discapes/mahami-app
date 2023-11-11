7830ed06afe84a83e956d49e3d38adcd0e318e9b
Dennis Magnusson <magnden@gmail.com>
2023-11-11 19:42:07 +0200

    add SectionContainer and modify Section

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -14,44 +14,26 @@ export default function Section({
 }: SectionProps): JSX.Element {
 	return (
 		<View style={styles.sectionContainer}>
-			<Text
-				style={[
-					styles.sectionTitle,
-					{
-						color: theme.colors.text,
-					},
-				]}>
-				{title}
-			</Text>
-			<Text
-				style={[
-					styles.sectionDescription,
-					{
-						color: theme.colors.text,
-					},
-				]}>
-				{children}
-			</Text>
+			<Text style={styles.sectionTitle}>{title}</Text>
+			<Text style={styles.sectionDescription}>{children}</Text>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
 	sectionContainer: {
-		marginTop: 32,
-		paddingHorizontal: 24,
+		padding: 12,
 		backgroundColor: theme.colors.element,
-		margin: 20,
-		borderRadius: 100,
+		borderRadius: 20,
 	},
 	sectionTitle: {
 		fontSize: 24,
-		fontWeight: '600',
+		color: theme.colors.text,
 	},
 	sectionDescription: {
 		marginTop: 8,
 		fontSize: 18,
-		fontWeight: '400',
+		color: theme.colors.text,
 	},
 	highlight: {
 		fontWeight: '700',
---
+++ src/components/SectionContainer.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import type { PropsWithChildren } from 'react';
+
+type SectionContainerProps = PropsWithChildren;
+
+export default function SectionContainer({
+	children,
+}: SectionContainerProps): JSX.Element {
+	return <View style={styles.container}>{children}</View>;
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		flexDirection: 'column',
+		gap: 10,
+		padding: 10,
+	},
+});
