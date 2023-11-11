5a4f3474cb3e3ed409c5ec17be345e02eb752bd2
Severi <severi.lybeck@gmail.com>
2023-11-11 19:31:51 +0000

    fixed 'minutes' tab

--- src/screens/MinutesScreen.tsx
+++ src/screens/MinutesScreen.tsx
@@ -15,17 +15,17 @@ export default function PointsScreen(): JSX.Element {
 		<ScrollView contentInsetAdjustmentBehavior="automatic">
 			<View>
 				<SectionContainer>
-					<Section title="Sleep" titleStyle={{color: theme.colors.sleep}} data="45 minutes">
-						<Text>Your sleep score for the last night was 86 which earns you <Text style={{color: theme.colors.sleep}}>45</Text> minutes</Text>
+					<Section title="Sleep" titleStyle={{color: theme.colors.blue}} data="45 minutes">
+						<Text style={styles.text}>Your sleep score for the last night was 86 which earns you <Text style={{color: theme.colors.blue}}>45</Text> minutes</Text>
 					</Section>
-					<Section title="Steps" titleStyle={{color: theme.colors.steps}} data="39 minutes">
-						<Text>You have taken 5914 steps today, which earns you <Text style={{color: theme.colors.steps}}> 39</Text> minutes</Text>
+					<Section title="Steps" titleStyle={{color: theme.colors.green}} data="39 minutes">
+						<Text style={styles.text}>You have taken 5914 steps today, which earns you <Text style={{color: theme.colors.green}}> 39</Text> minutes</Text>
 					</Section>
-					<Section title="Excercise" titleStyle={{color: theme.colors.excercise}}  data="20 minutes">
-						<Text>You have completed 63% of your excercise goal, which earns you <Text style={{color: theme.colors.excercise}}>20</Text> minutes.</Text>
+					<Section title="Excercise" titleStyle={{color: theme.colors.orange}}  data="20 minutes">
+						<Text style={styles.text}>You have completed 63% of your excercise goal, which earns you <Text style={{color: theme.colors.orange}}>20</Text> minutes.</Text>
 					</Section>
 					<Section title="How it works" >
-						<Text>You can earn more screen time by completing activities, walking and meeting other goals that you have agreed upon together wtih your guardian. {"\n\n"}Your guardian can set a screentime limit and change how you get points.</Text>
+						<Text style={styles.text}>You can earn more screen time by completing activities, walking and meeting other goals that you have agreed upon together wtih your guardian. {"\n\n"}Your guardian can set a screentime limit and change how you get points.</Text>
 					</Section>
 				</SectionContainer>
 			</View>
