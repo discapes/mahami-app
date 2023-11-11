36d316246f8a4e2c4c6ab743a032038cf89b6eac
Dennis Magnusson <magnden@gmail.com>
2023-11-11 15:02:30 +0200

    install react navigation

--- android/app/src/main/java/com/mahamimobile/MainActivity.java
+++ android/app/src/main/java/com/mahamimobile/MainActivity.java
@@ -1,5 +1,8 @@
 package com.mahamimobile;
 
+// React Navigation
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
@@ -28,5 +31,10 @@ public class MainActivity extends ReactActivity {
         getMainComponentName(),
         // If you opted-in for the New Architecture, we enable the Fabric Renderer.
         DefaultNewArchitectureEntryPoint.getFabricEnabled());
+  }
+  // React Navigation
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
